5c82c1c2354b5db8a7373c5b9f0dadcd2651b7e4
Agnesxxw <Agnes_xxw@outlook.com>
2020-04-21 20:18:04 +0800

    change msg

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ const express = require("express");
 var app = express();
 
 app.get("/",(req, res)=>{
-    res.send({hi:"there"});
+    res.send("hello everyone!");
 })
 
 const PORT = process.env.PORT || 3000 // if running on a local machine, env.port won't work
