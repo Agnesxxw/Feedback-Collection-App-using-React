3dd7db98dd0b9e94a8e48503333ab13525e5718b
Agnesxxw <Agnes_xxw@outlook.com>
2020-04-22 17:46:32 +0800

    change authentication into github since firewall

--- server/index.js
+++ server/index.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GithubStrategy = require("passport-github").Strategy;
 const keys = require("./config/keys");
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID:keys.googleClientID,
-    clientSecret:keys.googleClientSecret,
-    callbackURL:'/auth/google/callback'
-}, (accessToken)=>{
-    console.log(accessToken);
+passport.use(new GithubStrategy({
+    clientID:keys.githubClientID,
+    clientSecret:keys.githubClientSecret,
+    callbackURL:'/auth/github/callback'
+}, (accessToken, refreshToken, profile, done)=>{
+    console.log('refresh token', refreshToken);
+    console.log('profile', profile);
+    console.log('access token', accessToken);
+
     })
 ); // tell passport to use googleStrategy in our application
 
 app.get(
-    "/auth/google", // route handler
-    passport.authenticate('google',{
+    "/auth/github", // route handler
+    passport.authenticate('github',{
     scope:['profile','email']
-    })
+    }, {failWithError: true})
 );
 
+app.get("/auth/github/callback", passport.authenticate('github'));
 
 
 const PORT = process.env.PORT || 5000 // if running on a local machine, env.port won't work
