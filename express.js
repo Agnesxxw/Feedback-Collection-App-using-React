1e2b3a84d1f061fa5f7232b97226670ededdd211
Agnesxxw <Agnes_xxw@outlook.com>
2020-04-22 16:10:47 +0800

    finish auth

--- express.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-var app = express();
-
-app.get("/",(req, res)=>{
-    res.send("hello everyone!");
-})
-
-const PORT = process.env.PORT || 3000 // if running on a local machine, env.port won't work
-app.listen(PORT);
